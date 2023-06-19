77dbebe1c8f47cc8f8a3efd6a063332158b8b2a3
zjowowen <zjowowen@outlook.com>
2023-06-20 00:32:59 +0800

    add conditioned cov

--- SACLSP/models/common/__init__.py
+++ SACLSP/models/common/__init__.py
@@ -1,8 +1,8 @@
-from .parameter import NonegativeParameter, TanhParameter, CovarianceMatrix
+from .parameter import NonegativeParameter, TanhParameter
 from .cnf import CNF
 from .gmm import GMM
 from .realnvp import RealNVP
 from .mlp import MLP
 from .gaussian import Gaussian, GaussianTanh
 from .function import NonegativeFunction, TanhFunction
-
+from .matrix import CovarianceMatrix
